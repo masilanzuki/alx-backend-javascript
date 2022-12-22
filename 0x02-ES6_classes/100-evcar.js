5167c68c0ea4bf985e74cb6eb099e710c82d1d96
masilanzuki <massilaeugene@gmail.com>
2022-12-22 20:38:29 +0300

    okay

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -9,13 +9,10 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
- 
-
   cloneCar() {
     const NewObj = this.constructor[Symbol.species] || this.constructor;
     const clone = new NewObj();
     return clone;
   }
-
-  
+ 
 }
