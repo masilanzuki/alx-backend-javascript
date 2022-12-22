778c43a6d5a842e607ad4a3d3c4c1e3d9224d4fb
masilanzuki <massilaeugene@gmail.com>
2022-12-22 20:54:55 +0300

    nn

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,39 +1,13 @@
-export default class Car {
-  constructor(brand, motor, color) {
-    this.brand = brand;
-    this.motor = motor;
-    this.color = color;
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
   }
 
-  get brand() {
-    return this._brand;
-  }
-
-  set brand(brand) {
-    this._brand = brand;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(motor) {
-    this._motor = motor;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(color) {
-    this._color = color;
-  }
-
-  static get [Symbol.species]() {
-    return this;
-  }
-
+  // eslint-diable-next-line
   cloneCar() {
-    return new this.constructor[Symbol.species]();
+    return new Car[Symbol.species]();
   }
 }
